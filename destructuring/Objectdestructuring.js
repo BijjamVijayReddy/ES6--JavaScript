eae5ec3efdbf14bd89e2a51e48979c54b9603d69
Bijjamvijayreddy <bijjamvijayreddy@gmail.com>
2023-08-07 14:27:18 +0530

    Object destructuring description added

--- destructuring/Objectdestructuring.js
+++ destructuring/Objectdestructuring.js
@@ -7,3 +7,8 @@ var{firstName}=person;
 var{secondName}=person;
 console.log(firstName,secondName)
 console.log(secondName)
+
+
+// Object destructuring is convenient way to get values from object and assign to variables.
+
+// Object destructuring is particularly useful when you have an object with multiple properties, and you only need a few of them for further processing.
