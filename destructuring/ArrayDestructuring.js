2587d9e74a82e6fb54c6b5e93801aa546b97cdb3
Bijjamvijayreddy <bijjamvijayreddy@gmail.com>
2023-08-07 14:32:40 +0530

    Array Destructuring

---
+++ destructuring/ArrayDestructuring.js
@@ -0,0 +1,10 @@
+
+
+var array=["Vijay","Bijjam","React"]
+
+var [fName,sName] = array
+
+console.log(fName)
+console.log(sName)
+
+// same as Object destructuring.But here values or properties are assign based on index value.
